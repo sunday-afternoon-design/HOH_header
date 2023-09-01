538c9e91b9b6fa90207fcee0bf131bd886862bae
zooofzane <yu_an12@163.com>
2023-09-01 00:12:34 -0400

    9/1 update

--- j.js
+++ j.js
@@ -15,9 +15,14 @@ document.addEventListener("DOMContentLoaded", function () {
         .classList
         .add("no-scroll");
 
-        circle.setAttribute("x", mousex - currentWidth / 2);
-        circle.setAttribute("y", mousey - currentWidth / 2);
+        function initializeCirclePosition() {
+            circle.setAttribute("x", window.innerWidth / 2 - currentWidth / 2);
+            circle.setAttribute("y", window.innerHeight / 2 - currentWidth / 2);
+          }
         
+          // Call the function to initialize the circle position
+          initializeCirclePosition();
+
     // random shape
     let lastIndex = -1;
     function changeSvgLinkRandomly() {
@@ -39,24 +44,26 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // cursor following
     function tick() {
-    // Calculate SVG coordinates based on mouse position
-    let maskTargetX = mousex - currentWidth / 2;
-    let maskTargetY = mousey - currentWidth / 2;
-  
-    // Interpolate to target position
-    let currentX = parseFloat(circle.getAttribute("x"));
-    let currentY = parseFloat(circle.getAttribute("y"));
-  
-    let circleX = lerp(currentX, maskTargetX, 0.3);
-    let circleY = lerp(currentY, maskTargetY, 0.3);
-  
-    // Update the x, y position
-    circle.setAttribute("x", circleX);
-    circle.setAttribute("y", circleY);
-  
-    // Continue the animation
-    window.requestAnimationFrame(tick);
-  }
+        if(random_mask_lock){
+            console.log(random_mask_lock)
+        // Calculate SVG coordinates based on mouse position
+        let maskTargetX = mousex - currentWidth / 2;
+        let maskTargetY = mousey - currentWidth / 2;
+      
+        // Interpolate to target position
+        let currentX = parseFloat(circle.getAttribute("x"));
+        let currentY = parseFloat(circle.getAttribute("y"));
+      
+        let circleX = lerp(currentX, maskTargetX, 0.3);
+        let circleY = lerp(currentY, maskTargetY, 0.3);
+      
+        // Update the x, y position
+        circle.setAttribute("x", circleX);
+        circle.setAttribute("y", circleY);
+    }
+        // Continue the animation
+        window.requestAnimationFrame(tick);
+      }
     tick()
 
     document.addEventListener("mousemove", function (event) {
@@ -82,30 +89,32 @@ document.addEventListener("DOMContentLoaded", function () {
             // let newX = centerX - 0.3 * currentWidth;
             // let newY = centerY - 0.1 * currentWidth;9            // circle.setAttribute("x", centerX);
             // circle.setAttribute("y", centerY);
+            increment+=10
             const newX = parseFloat(circle.getAttribute("x")) - 0.5 * increment;
             const newY = parseFloat(circle.getAttribute("y")) - 0.5 * increment;
         
             circle.setAttribute("x", newX);
             circle.setAttribute("y", newY);
-            
-            currentWidth = lerp(currentWidth,maxRadius,.9);
+      
+            currentWidth += increment;
             circle.setAttribute("width", currentWidth);
             
             requestAnimationFrame(animateMaskExpansion);
-
+            console.log("expand")
         } else {
             let rect = circle.getBoundingClientRect();
             currentWidth = maxRadius; // Reset to maxRadius
             circle.setAttribute("x", -0.2 * rect.width);
             circle.setAttribute("y", -0.2 * rect.height);
             circle.setAttribute("href", svgFiles[2]);
+            console.log("noexpand")
         }
         random_mask_lock = false;
         document
             .body
             .classList
             .remove("no-scroll");
-        console.log("expand")
+       
     }
 
     function lerp(a, b, t) {
